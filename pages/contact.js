e76778a42865e279ca78e08bda0567416b0a3bd2
Teddy-San <teseraphin@laposte.net>
2022-07-03 20:16:36 +0200

    correctifs

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,44 @@
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 
-const contact = () => {
-    return (
-      <div className="text-center mt-20">
-          <Meta title="Contactez-nous" />
-          <h1 className="text-4xl font-bold">Contactez-nous</h1>
-      </div>
-    )
+function ContactForm() {
+  const [state, handleSubmit] = useForm("xlezkgqv");
+  if (state.succeeded) {
+      return <p>Merci de nous joindre!</p>;
   }
-  
-  export default contact;
+  return (
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Votre email
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        Soumettre
+      </button>
+    </form>
+  );
+}
+function App() {
+  return (
+    <ContactForm />
+  );
+}
+export default App;
